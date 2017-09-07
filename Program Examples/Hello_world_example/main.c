8e162250d8cdaca9862e19e3610676e2dc2b042e
Steffen Rahbek Vutborg <svu@ucn.dk>
2017-09-07 11:21:26 +0200

    Added comments

--- Program Examples/Hello_world_example/main.c
+++ Program Examples/Hello_world_example/main.c
@@ -1,8 +1,16 @@
+/*
+    Standard hello world program
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The following line is a comment */
+// This is the main function. This is the entry point of the program
 int main()
 {
+    // Prints the message hello world! in the console
     printf("Hello world!\n");
+
+    // Returns 0 to indicate that all went as planned
     return 0;
-}
+} // End of the main function
